b12b24c3f9a0185c81d895e8f194809c4a036c4b
Henry Otis <henrykotis@gmail.com>
2026-03-14 11:45:29 -0600

    fix dangling semi colon

--- src/main/resources/assets/nvidium/shaders/terrain/translucent/mesh.glsl
+++ src/main/resources/assets/nvidium/shaders/terrain/translucent/mesh.glsl
@@ -42,7 +42,7 @@ taskPayloadSharedEXT Task taskIn;
 #define originAndBaseData taskIn.originAndBaseData
 #define quadCount taskIn.quadCount
 #define jiggle taskIn.jiggle
-#define translucencyIndex taskIn.translucencyIndex;
+#define translucencyIndex taskIn.translucencyIndex
 #else
 taskNV in Task { TASK_FIELDS };
 #endif
